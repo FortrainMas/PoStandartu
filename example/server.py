f3ec43e9e953d985b7f29ae3649d8e55327eab19
Ivan Shebanov <i.shebanov@g.nsu.ru>
2025-06-02 21:00:16 +0700

    Some better style

--- example/server.py
+++ example/server.py
@@ -3,24 +3,33 @@ from PoStandartu.actions import Action
 from PoStandartu.actions.special_actions import MatchAction, StaticAction
 from PoStandartu.http_utils import Response, Request, RequestPattern
 
-import os
-static_path = os.path.join(os.getcwd(), "example", "static")
-print(f"Static is searched in {static_path}")
 
+#Созадайте сервер, а потом декларативно опишите в нём пути
 server = Server("127.0.0.1", 8080)
 
-server.actions.register_action(
-    Action("/hello", lambda request: Response(status_code=200, content="Hello, World!"))
-)
-server.actions.register_action(
-    Action("/hello?name=<name>", lambda request: Response(status_code=200, content=f"Hello, {request.params['name']}!"))
-)
+# Легко можно создавать пути
+server.action("/hello", lambda request: Response(status_code=200, content="Hello, World!"))
+# Даже с параметрами, которые автоматически будет спаршены из запроса пользователя
+server.action("/hello?name=<name>", lambda request: Response(status_code=200, content=f"Hello, {request.params['name']}!"))
+
+
+# Нужно раздать статику?
+# Не беда, достаточно использовать встроенную утилиту, которая выполнит все за вас
+# Просто создайте экшен, который будет использовать файлы в вашей папке static_path
+import os
+static_path = os.path.join(os.getcwd(), "example", "static")
 server.actions.register_action(
     StaticAction(static_path=static_path)
 )
+
+# А что если ничего не подошло и ни один паттерн не сработал?
+# И здесь PoStandartu не оставит вас в беде
+# Используйте специальный экшен MatchAction. Он всегда вызовется вне зависимости от того, что запросил пользователь
+# Очень удобно возвращать 404
 server.actions.register_action(
     MatchAction(
         lambda request: Response(status_code=404, content="404 Not Found"))
 )
 
+# Всё. Можно запускать и ждать клиентиков. Всё будет хорошо работать, наверное
 server.run()
--- src/PoStandartu/server.py
+++ src/PoStandartu/server.py
@@ -1,7 +1,7 @@
 import socket
 import selectors
 from PoStandartu.server_utils import ServerSocket
-from PoStandartu.actions import Actions
+from PoStandartu.actions import Actions, Action
 
 HOST, PORT = '127.0.0.1', 8080
 
@@ -12,6 +12,8 @@ class Server:
         self.port = port
         self.selector = selectors.DefaultSelector()
         self.actions = Actions()
+    def action(self, pattern, action):
+        self.actions.register_action(Action(pattern, action))
     def run(self):
         server_socket = ServerSocket(HOST, PORT, self)
         print(f"Serving on http://{HOST}:{PORT}")
